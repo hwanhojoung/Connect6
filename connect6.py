40eee4851e348334138a125f0987ef8543692727
hwanhojoung <hwanhodevelop@gmail.com>
2023-11-08 01:33:56 +0900

    AI가 두는 기능까지 완료. 학습 데이터 생성 필요

--- connect6.py
+++ connect6.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
+import tensorflow as tf
+import numpy as np
 
 width = 800
 height = 800
@@ -29,11 +31,29 @@ toggle_button.pack()
 
 boardArray = [[0 for j in range(25)] for i in range(25)]
 
+# Create a simple model
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(25, 25)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(625, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model with random data
+random_board_states = np.random.randint(3, size=(1000, 25, 25))
+random_next_moves = np.random.randint(625, size=(1000,))
+
+model.fit(random_board_states, random_next_moves, epochs=5)
+
 def clearBoard():
     global boardArray
     boardArray = [[0 for j in range(25)] for i in range(25)]
 
 def updateBoard():
+    global turn
     canvas.delete('all')
     canvas.create_rectangle(0, 0, width, height, fill="#ffcc66")
     for i in range(25):
@@ -59,6 +79,31 @@ def updateBoard():
         canvas.create_oval(blank + point[0] * 32 - radius, blank + point[1] * 32 - radius,
                             blank + point[0] * 32 + radius, blank + point[1] * 32 + radius,
                             fill="#ff0000", outline="#ff0000")
+    if turn == 2:  # AI's turn
+        ai_move()
+
+def ai_move():
+    global turn, turnNum, isStart
+    board_state = np.array(boardArray).reshape((1, 25, 25))
+    predicted_move = model.predict(board_state).argmax()
+    resultPos = {'x': predicted_move // 25, 'y': predicted_move % 25}
+    # AI가 놓을 수 있는 곳을 선택할 때, 이미 돌이 놓인 곳이나 착수 금지점을 피하도록 예외 처리를 추가
+    while (resultPos['x'], resultPos['y']) in prohibited_points or boardArray[resultPos['x']][resultPos['y']] != 0:
+        predicted_move = (predicted_move + 1) % 625  # 다음 위치로 이동
+        resultPos = {'x': predicted_move // 25, 'y': predicted_move % 25}
+    boardArray[resultPos['x']][resultPos['y']] = turn
+    checkOmok(turn, resultPos['x'], resultPos['y'])
+    if not isStart:
+        turnNum = 2
+        turn = 3 - turn
+        isStart = True
+    else:
+        if turnNum == turn:
+            turnNum = 3 - turn
+        else:
+            turn = 3 - turn
+
+
 
 clearBoard()
 updateBoard()
@@ -111,6 +156,10 @@ def add_prohibited_point(xPos, yPos):
 def isClicked(xPos, yPos):
     global turn, turnNum, isStart
     resultPos = getMouseRoundPos(xPos, yPos)
+    if turn == 2:  # AI's turn
+        board_state = np.array(boardArray).reshape((1, 25, 25))
+        predicted_move = model.predict(board_state).argmax()
+        resultPos = {'x': predicted_move // 25, 'y': predicted_move % 25}
     if (resultPos['x'], resultPos['y']) not in prohibited_points and resultPos['x'] > -1 and resultPos['x'] < 25 and resultPos['y'] > -1 and resultPos['y'] < 25 and boardArray[resultPos['x']][resultPos['y']] == 0:
         boardArray[resultPos['x']][resultPos['y']] = turn
         checkOmok(turn, resultPos['x'], resultPos['y'])
@@ -142,7 +191,6 @@ def gameSet():
     prohibited_points.clear()  # 착수 금지점 초기화
     updateBoard()
     turn = 1
-
 
 def addOmok(turn, xPos, yPos, xDir, yDir):
     if xPos + xDir < 0 or xPos + xDir > 24 or yPos + yDir < 0 or yPos + yDir > 24:
